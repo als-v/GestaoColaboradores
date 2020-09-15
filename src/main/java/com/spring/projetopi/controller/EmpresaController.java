c336abafe3c488d6e61c2eeb65e37a7e9ed39b64
als-v <alisson.v3@hotmail.com>
2020-09-15 15:52:50 -0300

    Adição controller para direcionar a pagina/criação view edição

--- src/main/java/com/spring/projetopi/controller/EmpresaController.java
+++ src/main/java/com/spring/projetopi/controller/EmpresaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -21,6 +22,10 @@ public class EmpresaController {
 	@Autowired
 	EmpresaService empresaService;
 	
+	/*
+	 * Listagem das empresas (apenas para fins de teste)
+	 */
+	
 	@RequestMapping(value = "/empresas", method = RequestMethod.GET)
 	public ModelAndView getEmpresa() {
 		ModelAndView mv = new ModelAndView("empresas");
@@ -28,6 +33,10 @@ public class EmpresaController {
 		mv.addObject("empresas", empresas);
 		return mv;
 	}
+	
+	/* 
+	 * Cadastro de empresa
+	 */
 	
 	@RequestMapping(value = "/cadastro", method = RequestMethod.GET)
 	public String getEmpresaForm() {
@@ -43,5 +52,17 @@ public class EmpresaController {
 		
 		empresaService.save(empresa);	
 		return "redirect:/menuEmpresa/" + empresa.getEmpresa_id();
+	}
+	
+	/*
+	 * Edição da empresa
+	 */
+	
+	@RequestMapping(value = "/editarEmpresa/{id}", method = RequestMethod.GET)
+	public ModelAndView editarEmpresa(@PathVariable("id") Long id) {
+		ModelAndView mv = new ModelAndView("editarEmpresa");
+		Empresa empresa = empresaService.findById(id);
+		mv.addObject("empresa", empresa);
+		return mv;
 	}
 }
